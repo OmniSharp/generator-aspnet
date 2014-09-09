c80cdd738eee2c338a103e83ca10dccc87f4e0f4
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2014-09-09 10:26:23 -0700

    update to always refresh git repo when calling .remote. related to #9

--- app/index.js
+++ app/index.js
@@ -60,9 +60,9 @@ var AspnetGenerator = yeoman.generators.Base.extend({
     retrieveContent: function () {
         var done = this.async();
 
-        this.remote('ligershark', 'aspnet_vnext_samples', function(err, remote) {
+        this.remote('ligershark', 'aspnet_vnext_samples', 'master', function(err, remote) {
             done();
-        });
+        },true);
     },
 
     writing: function () {
