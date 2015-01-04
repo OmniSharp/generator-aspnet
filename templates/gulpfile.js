56130ad4a959c4b9030b06f523f6c4f1cbc1c567
Shayne Boyer <shayne.p.boyer@disney.com>
2015-01-04 17:54:40 -0500

    Adding alias for JavaScript, support for TypeScript and GulpFile

---
+++ templates/gulpfile.js
@@ -0,0 +1,12 @@
+/*
+This file is the main entry point for defining Gulp tasks and using Gulp plugins
+To learn more visit: https://github.com/gulpjs/gulp/blob/master/docs/README.md
+*/
+'use strict';
+
+var gulp = require('gulp');
+
+// The default task (called when you run `gulp` from CLI)
+gulp.task('default', function() {
+  console.log('Running default Gulp task: replace with your own code!');
+});
