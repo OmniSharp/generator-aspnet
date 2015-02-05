20433874b62c75fe0666f319fc09409cd36f2194
Shayne Boyer <shayne.p.boyer@disney.com>
2015-02-05 14:58:33 -0500

    Removed .remote(), added mocha test coverage for all application types and sub generators. Added scripts section and npm test for Travis CI support.

---
+++ samples/empty/Startup.cs
@@ -0,0 +1,14 @@
+using System;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+
+namespace <%= namespace %>
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        }
+    }
+}
---
+++ test/subgenerators.js
@@ -0,0 +1,138 @@
+var util = require('./test-utility');
+
+/*
+ * Test for all subgenerators NOT requiring a name argument
+ */
+describe('Subgenerators without arguments tests', function () {
+
+    describe('aspnet:PackageJson', function () {
+        util.goCreate('PackageJson');
+        util.fileCheck('should create package json file', 'package.json');
+    });
+
+    describe('aspnet:Gulpfile', function () {
+        util.goCreate('GulpFile');
+        util.fileCheck('should create gulp file', 'gulpfile.js');
+    });
+
+    describe('aspnet:BowerJson', function () {
+        util.goCreate('BowerJson');
+        util.fileCheck('should create bower file', 'bower.json');
+    });
+
+    describe('aspnet:Config', function () {
+        util.goCreate('Config');
+        util.fileCheck('should create config json file', 'config.json');
+    });
+
+    describe('aspnet:StartupClass', function () {
+        util.goCreate('StartupClass');
+        util.fileCheck('should create Startup.cs file', 'Startup.cs');
+    });
+});
+
+/*
+ * Test for all subgenerators requiring a name argument
+ */
+describe('Subgenerators with named arguments tests', function () {
+
+    describe('aspnet:Class', function () {
+        var arg = 'MyClass';
+        var filename = 'MyClass.cs';
+
+        util.goCreateWithArgs('Class', [arg]);
+        util.fileCheck('should create ' + filename + ' file', filename);
+        util.fileContentCheck(filename, 'Check file content', /[ ]*public[ ]*class[ ]*MyClass/);
+
+    });
+
+    describe('aspnet:CoffeeScript', function () {
+        var arg = 'file';
+        var filename = 'file.coffee';
+
+        util.goCreateWithArgs('CoffeeScript', [arg]);
+        util.fileCheck('should create ' + filename + ' file', filename);
+
+    });
+
+    describe('aspnet:Gruntfile', function () {
+        var arg = 'gruntfile';
+        var filename = 'gruntfile.js';
+
+        util.goCreateWithArgs('Gruntfile', [arg]);
+        util.fileCheck('should create ' + filename + ' file', filename);
+
+    });
+
+    describe('aspnet:HTMLPage', function () {
+        var arg = 'mypage';
+        var filename = 'mypage.html';
+
+        util.goCreateWithArgs('HTMLPage', [arg]);
+        util.fileCheck('should create ' + filename + ' file', filename);
+
+    });
+
+    describe('aspnet:JavaScript', function () {
+        var arg = 'file';
+        var filename = 'file.js';
+
+        util.goCreateWithArgs('JavaScript', [arg]);
+        util.fileCheck('should create ' + filename + ' file', filename);
+
+    });
+
+    describe('aspnet:JSON', function () {
+        var arg = 'file';
+        var filename = 'file.json';
+
+        util.goCreateWithArgs('JSON', [arg]);
+        util.fileCheck('should create ' + filename + ' file', filename);
+
+    });
+
+    describe('aspnet:MvcController', function () {
+        var arg = 'file';
+        var filename = 'file.cs';
+
+        util.goCreateWithArgs('MvcController', [arg]);
+        util.fileCheck('should create ' + filename + ' file', filename);
+
+    });
+
+    describe('aspnet:MvcView', function () {
+        var arg = 'file';
+        var filename = 'file.cshtml';
+
+        util.goCreateWithArgs('MvcView', [arg]);
+        util.fileCheck('should create ' + filename + ' file', filename);
+
+    });
+
+    describe('aspnet:TextFile', function () {
+        var arg = 'file';
+        var filename = 'file.txt';
+
+        util.goCreateWithArgs('TextFile', [arg]);
+        util.fileCheck('should create ' + filename + ' file', filename);
+
+    });
+
+    describe('aspnet:TypeScript', function () {
+        var arg = 'file';
+        var filename = 'file.ts';
+
+        util.goCreateWithArgs('TypeScript', [arg]);
+        util.fileCheck('should create ' + filename + ' file', filename);
+
+    });
+
+    describe('aspnet:WebApiController', function () {
+        var arg = 'file';
+        var filename = 'file.cs';
+
+        util.goCreateWithArgs('WebApiController', [arg]);
+        util.fileCheck('should create ' + filename + ' file', filename);
+
+    });
+});
