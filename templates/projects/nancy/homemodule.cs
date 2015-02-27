691feb7c852ab25935cc3c526dbc21ce4e46c421
Lars Wilhelmsen <lars@sral.org>
2015-02-27 08:50:03 +0100

    Added missing namespace template fields to Startup.cs & HomeModule.cs

--- templates/projects/nancy/homemodule.cs
+++ templates/projects/nancy/homemodule.cs
@@ -1,4 +1,4 @@
-namespace WebApplication1
+namespace <%= namespace %>
 {
     using Nancy;
     
--- templates/projects/nancy/startup.cs
+++ templates/projects/nancy/startup.cs
@@ -1,4 +1,4 @@
-namespace WebApplication1
+namespace <%= namespace %>
 {
     using Microsoft.AspNet.Builder;
     using Nancy.Owin;
