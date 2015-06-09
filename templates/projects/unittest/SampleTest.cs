daa7daf01f2104d876ce7accbfbf51cad6f17839
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2015-06-09 20:26:39 +0200

    Move content of unittest to projects templates

---
+++ templates/projects/unittest/SampleTest.cs
@@ -0,0 +1,12 @@
+using Xunit;
+namespace SampleTests.Tests
+{
+    public class MyTests
+    {
+        [Fact]
+        public void Test1()
+        {
+        	Assert.True(true);
+        }
+    }
+}
