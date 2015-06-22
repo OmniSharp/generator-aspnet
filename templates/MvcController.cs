2324f0badfa939ecd371a9d6bf27c88ce790bcf0
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2015-06-22 21:33:08 +0200

    Update MvcController template for beta5
    - content update
    - template character case update for consistency in project
    - code cleanup in subgenerator

--- templates/MvcController.cs
+++ templates/MvcController.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
