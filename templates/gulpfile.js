885c0be04f355ee5794775a79c9a56b438946fdc
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2015-06-26 23:33:12 +0200

    Update Gulpfile subgenerator for beta5
    - content review and update
    - update test to cover content of created file

--- templates/gulpfile.js
+++ templates/gulpfile.js
@@ -1,12 +1,11 @@
 /*
-This file is the main entry point for defining Gulp tasks and using Gulp plugins
-To learn more visit: https://github.com/gulpjs/gulp/blob/master/docs/README.md
+  This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+  Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
 */
 'use strict';
 
 var gulp = require('gulp');
 
-// The default task (called when you run `gulp` from CLI)
 gulp.task('default', function() {
-  console.log('Running default Gulp task: replace with your own code!');
+  // place code for your default task here
 });
