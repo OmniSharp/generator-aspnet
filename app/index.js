f35210b9befc45ce9f8d344d57c4c76d534c4967
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2015-09-11 21:15:36 +0200

    :memo: Add xUnit usage tip. Closes #342
    The xUnit project is missing usage tip
    that are shown to users when generator
    ends. This commit add short usage tip
    to fix this

--- app/index.js
+++ app/index.js
@@ -253,6 +253,7 @@ var AspnetGenerator = yeoman.generators.Base.extend({
     this.log(chalk.green('    dnu build') + ' (optional, build will also happen when it\'s run)');
     this.log(chalk.green('    dnx ConsoleApplication') + ' for console projects');
     this.log(chalk.green('    dnx kestrel') + ' or ' + chalk.green('dnx web') + ' for web projects');
+    this.log(chalk.green('    dnx test') + ' for unit test projects');
     this.log('\r\n');
 
   }
