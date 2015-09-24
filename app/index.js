780ff2567d609421b4fe24aecb16617a6d6cbaf9
Boyer, Shayne P <Shayne.P.Boyer@disney.com>
2015-09-24 12:19:08 -0400

    remove redudant files

--- app/index.js
+++ app/index.js
@@ -125,7 +125,7 @@ var AspnetGenerator = yeoman.generators.Base.extend({
 
         this.copy(this.sourceRoot() + '/project.json', this.applicationName + '/project.json');
 
-        this.copy(this.sourceRoot() + '/Dockerfile', this.applicationName + '/Dockerfile');
+        this.copy(this.sourceRoot() + '/../../Dockerfile.txt', this.applicationName + '/Dockerfile');
 
         /// wwwroot
         this.fs.copy(this.templatePath('wwwroot/**/*'), this.applicationName + '/wwwroot');
@@ -134,7 +134,7 @@ var AspnetGenerator = yeoman.generators.Base.extend({
       case 'webapi':
         this.sourceRoot(path.join(__dirname, '../templates/projects/' + this.type));
         this.fs.copy(this.sourceRoot() + '/../../gitignore.txt', this.applicationName + '/.gitignore');
-        this.fs.copy(this.sourceRoot() + '/Dockerfile', this.applicationName + '/Dockerfile');
+        this.copy(this.sourceRoot() + '/../../Dockerfile.txt', this.applicationName + '/Dockerfile');
         this.fs.copy(this.sourceRoot() + '/hosting.ini', this.applicationName + '/hosting.ini');
         this.fs.copyTpl(this.sourceRoot() + '/Startup.cs', this.applicationName + '/Startup.cs', this.templatedata);
         this.fs.copy(this.sourceRoot() + '/project.json', this.applicationName + '/project.json');
@@ -152,7 +152,7 @@ var AspnetGenerator = yeoman.generators.Base.extend({
           this.fs.copyTpl(this.templatePath('gulpfile.js'), this.applicationName + '/gulpfile.js', this.templatedata);
         }
         // individual files (configs, etc)
-        this.fs.copy(this.templatePath() + '/Dockerfile', this.applicationName + '/Dockerfile');
+        this.copy(this.sourceRoot() + '/../../Dockerfile.txt', this.applicationName + '/Dockerfile');
         this.fs.copy(this.templatePath('.bowerrc'), this.applicationName + '/.bowerrc');
         this.fs.copy(this.sourceRoot() + '/../../gitignore.txt', this.applicationName + '/.gitignore');
         this.fs.copyTpl(this.templatePath('bower.json'), this.applicationName + '/bower.json', this.templatedata);
@@ -194,7 +194,7 @@ var AspnetGenerator = yeoman.generators.Base.extend({
           this.fs.copyTpl(this.templatePath('gulpfile.js'), this.applicationName + '/gulpfile.js', this.templatedata);
         }
         // individual files (configs, etc)
-        this.fs.copy(this.templatePath() + '/Dockerfile', this.applicationName + '/Dockerfile');
+        this.copy(this.sourceRoot() + '/../../Dockerfile.txt', this.applicationName + '/Dockerfile');
         this.fs.copy(this.templatePath('.bowerrc'), this.applicationName + '/.bowerrc');
         this.fs.copy(this.sourceRoot() + '/../../gitignore.txt', this.applicationName + '/.gitignore');
         this.fs.copyTpl(this.templatePath('bower.json'), this.applicationName + '/bower.json', this.templatedata);
