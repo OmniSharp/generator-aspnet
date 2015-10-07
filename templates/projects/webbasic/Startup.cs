08d52ed95be8abab9f2b393908a97659ae51f716
Randy Patterson Jr <rpattjr@gmail.com>
2015-10-07 13:25:15 -0400

    Update WebBasic template for beta8. Closes #384.

--- templates/projects/webbasic/Startup.cs
+++ templates/projects/webbasic/Startup.cs
@@ -16,8 +16,9 @@ namespace <%= namespace %>
         public Startup(IHostingEnvironment env, IApplicationEnvironment appEnv)
         {
             // Setup configuration sources.
-            var builder = new ConfigurationBuilder(appEnv.ApplicationBasePath)
-                .AddJsonFile("config.json")
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(appEnv.ApplicationBasePath)
+                .AddJsonFile("appsettings.json")
                 .AddEnvironmentVariables();
             Configuration = builder.Build();
         }
@@ -44,16 +45,19 @@ namespace <%= namespace %>
 
             // Configure the HTTP request pipeline.
 
+            // Add the platform handler to the request pipeline.
+            app.UseIISPlatformHandler();
+
             // Add the following to the request pipeline only in development environment.
             if (env.IsDevelopment())
             {
-                app.UseErrorPage();
+                app.UseDeveloperExceptionPage();
             }
             else
             {
                 // Add Error handling middleware which catches all application specific errors and
                 // send the request to the following path or controller action.
-                app.UseErrorHandler("/Home/Error");
+                app.UseExceptionHandler("/Home/Error");
             }
 
             // Add static files to the request pipeline.
