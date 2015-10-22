91ebc7da664c788b4f9a5e663157d41128b2ac27
あまみや ゆうこ <1@1234.sh>
2015-10-22 08:25:17 +0800

    Update HomeController.cs

--- templates/projects/webbasic/Controllers/HomeController.cs
+++ templates/projects/webbasic/Controllers/HomeController.cs
@@ -29,7 +29,7 @@ namespace <%= namespace %>.Controllers
 
         public IActionResult Error()
         {
-            return View("~/Views/Shared/Error.cshtml");
+            return View();
         }
     }
 }
