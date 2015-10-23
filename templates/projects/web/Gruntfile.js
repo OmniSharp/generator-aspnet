c04fe469be7edb4d9cbe5e1831d2a281b076aa18
Scott Addie <tobias.addie@gmail.com>
2015-10-22 21:56:59 -0500

    Standardized the Gruntfile.js and gulpfile.js files
    Move 'use strict' statements to top of Gruntfile.js files

--- templates/projects/web/Gruntfile.js
+++ templates/projects/web/Gruntfile.js
@@ -1,7 +1,8 @@
 // This file in the main entry point for defining grunt tasks and using grunt plugins.
 // Click here to learn more. http://go.microsoft.com/fwlink/?LinkID=513275&clcid=0x409
-module.exports = function(grunt) {
-  'use strict';
+"use strict";
+
+module.exports = function(grunt) {  
   grunt.initConfig({
     bower: {
       install: {
