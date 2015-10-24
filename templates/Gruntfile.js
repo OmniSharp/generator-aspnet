79445eee8e67e1cc79c2be92f97ae7215794c22f
Scott Addie <tobias.addie@gmail.com>
2015-10-23 20:13:43 -0500

    Move 'use strict' statements to top of Gruntfile.js files

--- templates/Gruntfile.js
+++ templates/Gruntfile.js
@@ -2,9 +2,9 @@
   This file in the main entry point for defining grunt tasks and using grunt plugins.
   Click here to learn more. http://go.microsoft.com/fwlink/?LinkID=513275&clcid=0x409
 */
-module.exports = function(grunt) {
-  "use strict";
+"use strict";
 
+module.exports = function(grunt) {
   grunt.initConfig({
   });
 };
