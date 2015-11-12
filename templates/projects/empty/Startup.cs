fba86478fe035ca01812c697cca6e3e5682889a8
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2015-11-12 22:53:45 +0100

    Update Empty app template for rc1. Closes #464
    - update implementation
    - update project configuration file

--- templates/projects/empty/Startup.cs
+++ templates/projects/empty/Startup.cs
@@ -3,27 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
-using Microsoft.Framework.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace <%= namespace %>
 {
     public class Startup
     {
+        // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
         }
 
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
-            // Add the platform handler to the request pipeline.
             app.UseIISPlatformHandler();
-            
+
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
             });
         }
+
+        // Entry point for the application.
+        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
     }
 }
