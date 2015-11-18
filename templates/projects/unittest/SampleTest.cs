83c7667db9617eb3841091cd3b5028989a186036
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2015-11-18 20:47:58 +0100

    Update xUnit template project for RC1. Closes #497
    - add project.json fully based on xUnit example:
    - use implementation example based on xUnit example code:
    https://xunit.github.io/docs/getting-started-dnx.html
    
    This commit is based on xUnit RC1 release notes and updates:
    https://xunit.github.io/release-notes/2015-11-18.html

--- templates/projects/unittest/SampleTest.cs
+++ templates/projects/unittest/SampleTest.cs
@@ -5,12 +5,25 @@ using Xunit;
 
 namespace <%= namespace %>
 {
+    // see example explanation on xUnit.net website:
+    // https://xunit.github.io/docs/getting-started-dnx.html
     public class SampleTest
     {
         [Fact]
-        public void Test1()
+        public void PassingTest()
         {
-        	Assert.True(true);
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(5, Add(2, 2));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
         }
     }
 }
