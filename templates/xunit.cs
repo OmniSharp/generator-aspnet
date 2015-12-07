e6d4dbd572be48060043065b87dab8726da2ec79
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2015-12-07 21:16:02 +0100

    Add xUnit subgenerator template
    The content of template is based on existing VS addon:
    http://git.io/vRPrH
    @credits: @jsakamoto

---
+++ templates/xunit.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace <%= namespace %>
+{
+    public class <%= classname %>
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+        }
+    }
+}
