9558724a3263bfe12f3b0e1d1ac88e7d076e9001
Scott Addie <tobias.addie@gmail.com>
2015-12-09 22:11:58 -0600

    Fix broken links in Gruntfile.js files

--- templates/projects/webbasic/Gruntfile.js
+++ templates/projects/webbasic/Gruntfile.js
@@ -1,5 +1,7 @@
-// This file in the main entry point for defining grunt tasks and using grunt plugins.
-// Click here to learn more. http://go.microsoft.com/fwlink/?LinkID=513275&clcid=0x409
+/*
+  This file is the main entry point for defining grunt tasks and using grunt plugins.
+  Click here to learn more: https://docs.asp.net/en/latest/client-side/using-grunt.html
+*/
 "use strict";
 
 module.exports = function(grunt) {
