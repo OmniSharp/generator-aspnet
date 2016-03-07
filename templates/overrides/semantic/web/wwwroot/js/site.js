06ca0693f7911f9956114a5b8a5a1bbbfc0b2268
Josh Williams <josh.williams@praeses.com>
2016-03-06 18:03:43 -0600

    Changes - Add summary for MenuLinkTagHelper - Remove commented out code from VerifyCode.cshtml - Add full protocol link to web and webbasic Home/Index.cshtml, _Layout.cshtml, _ValidationScriptsPartial.cshtml - In _ValidationScriptsPartial.cshtml, change Production env to use semantic.validation.min.js - web/webbasic override bower.json indentation fix - In both _Layout.cshtml's (web and webbasic), add menu.header to be consistent with already available template. - Fix missing <%= namespace %> in html head title of _Layout.cshtml's - Use bootstraps .sr-only helper class for screenreaders. Add sr-only "Toggle Navigation" span for SR's. - Flatten site.css indentation - Update site.js code, add brackets to be consistent - Add referenced https://github.com/aspnet/Mvc/issues/2369 so people are aware of different solutions to issues with label/checkbox

--- templates/overrides/semantic/web/wwwroot/js/site.js
+++ templates/overrides/semantic/web/wwwroot/js/site.js
@@ -1,17 +1,16 @@
-﻿$(function () {
+$(function () {
     // create sidebar and attach to menu open
     $('.ui.sidebar')
-      .sidebar('attach events', '.toc.item')
-    ;
+        .sidebar('attach events', '.toc.item');
 
     //Hack to make MVC ValidationSummary tag helper play nicely with Semantic UI
     $('.ui.error ul').addClass('list');
 
-    if ($('.ui.error ul li:first').css('display') === 'none')
+    if ($('.ui.error ul li:first').css('display') === 'none') {
         $('.ui.error').hide();
-    else
+    } else {
         $('.ui.error').show();
-
+    }
 
     $('.ui.checkbox').checkbox();
 });
