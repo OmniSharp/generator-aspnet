e274a6f61ca310a95c255e1e769b31aebc22ac64
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-04-24 15:59:41 +0200

    :art: Add Program.cs generator for RC2. Closes #616
    This commit adds Program.cs entry point generator
    compatible with RC2 onwards.
    
    The content of Program.cs is based on
    content generated with `dotnet new`.
    This commits updates tests and usage
    information to cover that addition.
    
    Thanks!

---
+++ templates/Program.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace <%= namespace %>
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
