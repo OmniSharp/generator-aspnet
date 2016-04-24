6c43df2d7cadd2ca583007b4c1f8582438c97a77
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-04-24 19:26:19 +0200

    :arrow_up: Update TagHelper generator for RC2. Closes #619
    This commit introduces simple change to make TagHelper
    generator compatible with RC2 release.
    The changes includes changes introduced in aspnet/Mvc:
    aspnet/Announcements#89
    aspnet/Announcements#144
    
    Thanks!

--- templates/TagHelper.cs
+++ templates/TagHelper.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNet.Razor.Runtime.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
 
 namespace <%= namespace %>
 {
