356ca35a706000ba8f98862e858404d2134c3815
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-04-24 16:30:00 +0200

    :arrow_up: Update Nuget config generator for RC2. Closes #617
    This commit updates feed links used in prelease version
    to match one that works with dotnet.
    The tests checks are also updated to reflect this change.
    
    Thanks!

--- test/subgenerators.js
+++ test/subgenerators.js
@@ -113,7 +113,7 @@ describe('Subgenerators without arguments tests', function() {
     var filename = 'NuGet.config';
     util.fileCheck('should create NuGet configuration file', filename);
     util.fileContentCheck(filename, 'Check file content', /api\.nuget\.org/);
-    util.noFileContentCheck(filename, 'Check file content for no unstable feed', /https:\/\/myget\.org\/f\/aspnetrc1\/api\/v2/);
+    util.noFileContentCheck(filename, 'Check file content for no unstable feed', /https:\/\/www\.myget\.org\/F\/aspnetrelease\/api\/v3/);
   });
 
   // unstable feed should be found in generated file
@@ -122,7 +122,7 @@ describe('Subgenerators without arguments tests', function() {
     var filename = 'NuGet.config';
     util.goCreateWithArgs('nuget', [arg]);
     util.fileCheck('should create ' + filename + ' file with unstable feed', filename);
-    util.fileContentCheck(filename, 'Check file content for unstable feed', /https:\/\/myget\.org\/f\/aspnetrc1\/api\/v2/);
+    util.fileContentCheck(filename, 'Check file content for unstable feed', /https:\/\/www\.myget\.org\/F\/aspnetrelease\/api\/v3/);
   });
 
   describe('aspnet:readme creates README.md', function() {
