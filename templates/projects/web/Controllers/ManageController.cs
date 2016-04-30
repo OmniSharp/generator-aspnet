c1fddd04ac829a49790f5b4fe37adffb7bb20feb
Peter Blazejewicz <peterblazejewicz@users.noreply.github.com>
2016-05-01 00:23:39 +0200

    :arrow_up: Update web template to RC2
    This commit brings file content changes
    to web template (starter web with individual
    authorization (in aspnet/Templates convention)
    including:
    - namespace changes
    - project structure changes
    - RC2 related structure additions
    - assets changes
    - client side deps updates
    - test coverage update/rewrite
    - client side assets updates
    - SQLite EF updates
    
    The template now restores, builds and
    runs fine.
    There are some small fixes remaining for
    this tempalte to be done but they will
    be part of separte review PR

--- templates/projects/web/Controllers/ManageController.cs
+++ templates/projects/web/Controllers/ManageController.cs
@@ -2,14 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Security.Claims;
-using Microsoft.AspNet.Authorization;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using <%= namespace %>.Models;
+using <%= namespace %>.Models.ManageViewModels;
 using <%= namespace %>.Services;
-using <%= namespace %>.ViewModels.Manage;
 
 namespace <%= namespace %>.Controllers
 {
@@ -174,7 +173,7 @@ namespace <%= namespace %>.Controllers
         }
 
         //
-        // GET: /Manage/RemovePhoneNumber
+        // POST: /Manage/RemovePhoneNumber
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> RemovePhoneNumber()
@@ -292,8 +291,8 @@ namespace <%= namespace %>.Controllers
         {
             // Request a redirect to the external login provider to link a login for the current user
             var redirectUrl = Url.Action("LinkLoginCallback", "Manage");
-            var properties = _signInManager.ConfigureExternalAuthenticationProperties(provider, redirectUrl, User.GetUserId());
-            return new ChallengeResult(provider, properties);
+            var properties = _signInManager.ConfigureExternalAuthenticationProperties(provider, redirectUrl, _userManager.GetUserId(User));
+            return Challenge(properties, provider);
         }
 
         //
@@ -306,7 +305,7 @@ namespace <%= namespace %>.Controllers
             {
                 return View("Error");
             }
-            var info = await _signInManager.GetExternalLoginInfoAsync(User.GetUserId());
+            var info = await _signInManager.GetExternalLoginInfoAsync(await _userManager.GetUserIdAsync(user));
             if (info == null)
             {
                 return RedirectToAction(nameof(ManageLogins), new { Message = ManageMessageId.Error });
@@ -338,9 +337,9 @@ namespace <%= namespace %>.Controllers
             Error
         }
 
-        private async Task<ApplicationUser> GetCurrentUserAsync()
+        private Task<ApplicationUser> GetCurrentUserAsync()
         {
-            return await _userManager.FindByIdAsync(HttpContext.User.GetUserId());
+            return _userManager.GetUserAsync(HttpContext.User);
         }
 
         #endregion
--- templates/projects/web/Models/ApplicationUser.cs
+++ templates/projects/web/Models/ApplicationUser.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace <%= namespace %>.Models
 {
