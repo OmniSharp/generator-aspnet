1e1a9d8e3d6b0377cc77c4564293e69138d3a4d3
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-05-02 22:25:44 +0200

    :lock: Always use HTTPS for MS url shortener
    /cc @sayedihashimi

--- templates/gulpfile.js
+++ templates/gulpfile.js
@@ -1,6 +1,6 @@
 /*
   This file in the main entry point for defining Gulp tasks and using Gulp plugins.
-  Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+  Click here to learn more. https://go.microsoft.com/fwlink/?LinkId=518007
 */
 "use strict";
 
