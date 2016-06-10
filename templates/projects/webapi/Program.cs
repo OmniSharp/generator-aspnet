bb21bb1d8e5783f8678c16eebe3e5011a1a9b66b
Sergio Sisternes <sesispla@outlook.com>
2016-06-10 22:32:26 +0200

    Dockerfile for RC2. Closes #685 Added ConfigurationBuilder to WebHostBuilder for Dockerfile launch configuration

--- templates/projects/webapi/Program.cs
+++ templates/projects/webapi/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 
 namespace <%= namespace %>
 {
@@ -12,7 +13,13 @@ namespace <%= namespace %>
     {
         public static void Main(string[] args)
         {
+            var config = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .AddEnvironmentVariables(prefix: "ASPNETCORE_")
+                .Build();
+            
             var host = new WebHostBuilder()
+                .UseConfiguration(config)
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
