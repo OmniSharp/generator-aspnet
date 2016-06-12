c473bc0f8b0e8ea0e30596a79331e0ecf102d3b6
Sergio Sisternes <sesispla@outlook.com>
2016-06-12 21:33:37 +0200

    Added ConfigurationBuilder to Empty Project template. Closes #685

--- templates/projects/emptyweb/Program.cs
+++ templates/projects/emptyweb/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace <%= namespace %>
 {
@@ -11,7 +12,13 @@ namespace <%= namespace %>
     {
         public static void Main(string[] args)
         {
+           var config = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .AddEnvironmentVariables(prefix: "ASPNETCORE_")
+                .Build();
+            
             var host = new WebHostBuilder()
+                .UseConfiguration(config)
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
