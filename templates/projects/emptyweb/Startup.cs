1663ce5d748c70301b415a06c75ea1b2c5e19685
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-06-16 21:20:54 +0200

    Add logging and diagnostic pages to empty
    This commit brings changes from aspnet/Templates#611
    to empty web application template
    
    The comit contains some minor changes required resulted from
    adding changes to project not found in original
    aspnet/Templates#611 PR
    
    Thanks!

--- templates/projects/emptyweb/Startup.cs
+++ templates/projects/emptyweb/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace <%= namespace %>
 {
@@ -18,8 +19,15 @@ namespace <%= namespace %>
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
