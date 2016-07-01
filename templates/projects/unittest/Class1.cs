605d6e52873a730b5bcce61891c6fb97c3f761fd
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-07-01 21:10:21 +0200

    :art: Update xUnit project template
    This commit is a follow-up to discussion in:
    
    - project.json dependencies updated
    - the class renamed to Class1.cs to strictly follow
    xUnit project documentation
    - test coverage updated following filename change
    
    /cc @bradwilson
    
    Thanks!

--- templates/projects/unittest/Class1.cs
+++ templates/projects/unittest/Class1.cs
@@ -1,14 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace <%= namespace %>
 {
     // see example explanation on xUnit.net website:
     // https://xunit.github.io/docs/getting-started-dotnet-core.html
-    public class SampleTest
+    public class Class1
     {
         [Fact]
         public void PassingTest()
