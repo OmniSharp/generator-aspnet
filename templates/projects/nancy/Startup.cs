38ae195fb3dc79b483a3afb5040a42a0d1b682ac
Jonathan Channon <jonathan.channon@gmail.com>
2016-07-20 09:12:17 +0100

    Made Nancy project template netcoreapp1.0 compatible

--- templates/projects/nancy/Startup.cs
+++ templates/projects/nancy/Startup.cs
@@ -1,16 +1,13 @@
 namespace <%= namespace %>
 {
-    using Microsoft.AspNet.Builder;
+    using Microsoft.AspNetCore.Builder;
     using Nancy.Owin;
- 
+
     public class Startup
     {
         public void Configure(IApplicationBuilder app)
         {
             app.UseOwin(x => x.UseNancy());
         }
-
-        // Entry point for the application.
-        public static void Main(string[] args) => Microsoft.AspNet.Hosting.WebApplication.Run<Startup>(args);
     }
 }
