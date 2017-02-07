fd74b4aec936236e9ba52dc87bdb4e3f854a3292
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2017-02-07 20:46:55 +0100

    Startup code routine updates for RC

--- templates/projects/mvc/Program.cs
+++ templates/projects/mvc/Program.cs
@@ -1,10 +1,9 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 
 namespace <%= namespace %>
 {
@@ -12,13 +11,7 @@ namespace <%= namespace %>
     {
         public static void Main(string[] args)
         {
-            var config = new ConfigurationBuilder()
-                .AddCommandLine(args)
-                .AddEnvironmentVariables(prefix: "ASPNETCORE_")
-                .Build();
-
             var host = new WebHostBuilder()
-                .UseConfiguration(config)
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
